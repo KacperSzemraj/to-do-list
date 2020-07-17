15282762640e2b958ebd3dc249a36f3fd9ed1991
Kacper Szemraj <kacperprzemyslawszemraj@gmail.com>
2020-07-17 17:29:35 +0200

    sample tasks deleted

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
 {
-    const tasks = [
-        {
-            content: "Zadanie 1",
-            done: false,
-        },
-        {
-            content: "Zadanie 2",
-            done: true,
-        },
-    ];
+    const tasks = [];
 
     const addNewTask = (newTaskContent) => {
         tasks.push({
