90b65f8a59575db7cbe555432ebc0eb5212c9f88
Kacper Szemraj <kacperprzemyslawszemraj@gmail.com>
2020-07-20 22:51:17 +0200

    correction of miswriting

--- js/script.js
+++ js/script.js
@@ -88,7 +88,7 @@
     const renderButtons = () => {
         const buttonsElement = document.querySelector(".js-buttons");
 
-        if (!tasks.lenght) {
+        if (!tasks.length) {
             buttonsElement.innerHTML = "";
             return;
         }
